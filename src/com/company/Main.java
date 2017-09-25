7177b763b602afb7df5627522f749d5e930e258e
youwen000 <a0979636658@gmail.com>
2017-09-25 14:45:54 +0800

    modifide

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("106024042劉侑紋");
+        System.out.println("資傳系");
+    }
+}
